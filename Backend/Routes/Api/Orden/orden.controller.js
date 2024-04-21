442219aa6b1d6abd875801371883e6e45553669c
Daniel Salazar <dsnile.salazr@gmail.com>
2024-04-21 16:59:03 -0600

    Refactor crearOrden function in orden.controller.js to include user ID in newOrden object

--- Backend/Routes/Api/Orden/orden.controller.js
+++ Backend/Routes/Api/Orden/orden.controller.js
@@ -2,16 +2,9 @@
 const Orden = require('./orden.model');
 
 
-const status = (pago) =>{
-    
-}
-
 
 exports.crearOrden = (request, response) => {
-    const {Total, Estado, Ciudad, CodigoPostal, Colonia, Calle, Productos} = request.body;
-    const newOrden = new Orden({
-        Total, Estado,  Ciudad, CodigoPostal, Colonia, Calle, Productos
-    });
+    const newOrden = new Orden({...request.body, user : request.user._id  });
     newOrden.save().then(res => response.send(res)).catch(err => console.log(err));
 };
 
