ec47f1d146ecfbcfe0522b23fda6e4e9c45d2bfb
Daniel Salazar <dsnile.salazr@gmail.com>
2024-04-21 16:53:14 -0600

    Refactor crearOrden function in orden.controller.js

--- Backend/Routes/Api/Orden/orden.controller.js
+++ Backend/Routes/Api/Orden/orden.controller.js
@@ -1,10 +1,16 @@
 
 const Orden = require('./orden.model');
 
+
+const status = (pago) =>{
+    
+}
+
+
 exports.crearOrden = (request, response) => {
-    const {Total, EstatusPago, Estado, Ciudad, CodigoPostal, Colonia, Calle, Productos} = request.body;
+    const {Total, Estado, Ciudad, CodigoPostal, Colonia, Calle, Productos} = request.body;
     const newOrden = new Orden({
-        Total, Estado, EstatusPago, Ciudad, CodigoPostal, Colonia, Calle, Productos
+        Total, Estado,  Ciudad, CodigoPostal, Colonia, Calle, Productos
     });
     newOrden.save().then(res => response.send(res)).catch(err => console.log(err));
 };
