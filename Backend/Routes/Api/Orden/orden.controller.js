cbc91d7d60398d9c99828471d20582fe12e72a92
Daniel Salazar <dsnile.salazr@gmail.com>
2024-04-21 17:06:00 -0600

    Refactor updateOrden function in orden.controller.js to handle updating orders

--- Backend/Routes/Api/Orden/orden.controller.js
+++ Backend/Routes/Api/Orden/orden.controller.js
@@ -8,3 +8,22 @@ exports.crearOrden = (request, response) => {
     newOrden.save().then(res => response.send(res)).catch(err => console.log(err));
 };
 
+
+exports.updateOrden = async (request, response) => {
+    if(request.body._id){
+        delete request.body.id;
+    }
+    const ordenUpdate = await Orden.findByIdAndUpdate(request.query.id, request.body)
+
+    if(ordenUpdate) return response.send(ordenUpdate);
+
+    return response.json({message : 'error al actualizar'});
+}
+
+
+exports.mostrarOrdenes = async (request, response) => {
+    const ordenes = await Orden.find({});
+    if(!ordenes) return response.json({message : 'algo salio mal'});
+    return response.send(ordenes);
+}
+
