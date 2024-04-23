9398020b4de770a08a32f185ff6659a4dc641576
Daniel Salazar <dsnile.salazr@gmail.com>
2024-04-22 19:02:51 -0600

    Add API endpoint for deleting an order by ID in orden.controller.js

--- Backend/Routes/Api/Orden/index.js
+++ Backend/Routes/Api/Orden/index.js
@@ -6,3 +6,4 @@ const router = express.Router();
 router.get('/', controller.mostrarOrdenes);
 router.get('/:id', controller.mostrarOrdenPorId);
 router.put('/actualizar/:id', controller.updateOrden);
+router.delete('/eliminar/:id', controller.eliminarOrden);
