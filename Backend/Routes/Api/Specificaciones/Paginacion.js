91f34c2eec7d9fa00ab85352b5cc6fe8f88b912f
Daniel Salazar <dsnile.salazr@gmail.com>
2024-04-22 19:53:05 -0600

    Complete paginacion function to handle pagination in Paginacion.js

--- Backend/Routes/Api/Specificaciones/Paginacion.js
+++ Backend/Routes/Api/Specificaciones/Paginacion.js
@@ -1,8 +1,10 @@
 
-exports.paginacion = (request, Objeto) => {
-    const page = request.query.page*1 || 1;
-    const pageSize = request.query.pageSize*1 || 2;
+exports.paginacion = (Objeto, requestQuery) => {
+
+    const page = requestQuery.query.page*1 || 1;
+    const pageSize = requestQuery.query.pageSize*1 || 2;
     const skip = (page-1) * pageSize;
 
-    const objeto = Objeto.find()
+        return Objeto.skip(skip).limit(pageSize);
+
 }
