43259e254fea6d922923ce066f1f0aa28b9c2f57
Daniel Salazar <dsnile.salazr@gmail.com>
2024-04-22 20:12:16 -0600

    Refactor paginacion function to handle pagination in Paginacion.js

--- Backend/Routes/Api/Specificaciones/Paginacion.js
+++ Backend/Routes/Api/Specificaciones/Paginacion.js
@@ -1,10 +1,12 @@
 
-exports.paginacion = (Objeto, requestQuery) => {
+const paginacion = (Objeto, requestQuery) => {
 
-    const page = requestQuery.query.page*1 || 1;
-    const pageSize = requestQuery.query.pageSize*1 || 2;
+    const page = requestQuery.page*1 || 1;
+    const pageSize = requestQuery.pageSize*1 || 2;
     const skip = (page-1) * pageSize;
 
         return Objeto.skip(skip).limit(pageSize);
 
-}
+};
+
+module.exports = paginacion;
