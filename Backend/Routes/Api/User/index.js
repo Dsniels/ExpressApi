2581c1ee22550d200a8e32d5f693ecc35cfd9a86
Daniel Salazar <dsnile.salazr@gmail.com>
2024-04-22 20:02:30 -0600

    Add endpoint for retrieving all users with pagination

--- Backend/Routes/Api/User/index.js
+++ Backend/Routes/Api/User/index.js
@@ -5,6 +5,8 @@ const router = express.Router();
 
 router.get('/', auth.hasRole("user"), controller.show);
 
+router.get('/all', auth.hasRole("manager", controller.showUsers));
+
 router.post('/registrarse', controller.registerUser);
 
 router.post("/login", controller.loginUser);
