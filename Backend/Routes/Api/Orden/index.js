b9d3ce3d16725e8645343266bd5c4d52e98df7c3
Daniel Salazar <dsnile.salazr@gmail.com>
2024-04-22 21:33:49 -0600

    Refactor authentication routes and add authorization middleware in orden.index.js

--- Backend/Routes/Api/Orden/index.js
+++ Backend/Routes/Api/Orden/index.js
@@ -1,10 +1,12 @@
 const controller = require('./orden.controller');
 const express = require('express');
 const router = express.Router();
+const auth = require('../Auth/auth.service');
 
+router.get('/', auth.hasRole("manager") ,controller.mostrarOrdenes);
+router.get('/:id', auth.hasRole("user"), controller.mostrarOrdenPorId);
+router.put('/actualizar/:id', auth.hasRole("manager"), controller.updateOrden);
+router.delete('/eliminar/:id', auth.hasRole("manager")  , controller.eliminarOrden);
+router.post('/crearorden', auth.hasRole("user") ,controller.crearOrden);
 
-router.get('/', controller.mostrarOrdenes);
-router.get('/:id', controller.mostrarOrdenPorId);
-router.put('/actualizar/:id', controller.updateOrden);
-router.delete('/eliminar/:id', controller.eliminarOrden);
-router.post('/crearorden', controller.crearOrden);
+module.exports = router;
