de86e712651bfec204d23fdc5d2b766f9706fbec
Daniel Salazar <dsnile.salazr@gmail.com>
2024-04-22 19:02:34 -0600

    Add API endpoint for updating an order by ID in orden.controller.js

--- Backend/Routes/Api/Orden/index.js
+++ Backend/Routes/Api/Orden/index.js
@@ -5,3 +5,4 @@ const router = express.Router();
 
 router.get('/', controller.mostrarOrdenes);
 router.get('/:id', controller.mostrarOrdenPorId);
+router.put('/actualizar/:id', controller.updateOrden);
