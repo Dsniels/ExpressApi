c69d225809efa2bc26e848db11b7e80ff88cc1d3
Daniel Salazar <dsnile.salazr@gmail.com>
2024-04-23 20:41:04 -0600

    Update carrito.controller.js to handle cart functionality and update cart items

--- Backend/Routes/Api/Carrito/carrito.controller.js
+++ Backend/Routes/Api/Carrito/carrito.controller.js
@@ -1,5 +1,21 @@
+
 const redis = require('../../../config/redis');
 
 exports.obtenerCarritoId = (request, response) => {
+    const carritoID  = `carrito:${request.params.id}`;
+    redis.LRANGE(carritoID, 0 ,-1, (err, productos)=>{
+        if(err) return response.send(err);
+        const parseProducts = productos.map(JSON.parse);
+        return response.send(parseProducts);
+    });
+}
 
+exports.updateCarrito = (request, response) =>{ 
+    console.log(request);
+    const carritoID = `carrito:${request.auth.id}`;
+    const items = request.body.items
+    items.forEach(element => {
+        redis.rPush(carritoID, JSON.stringify(element)).then(()=>console.log('Items insertados: ', carritoID)).catch(err => console.log(err)); 
+    });
+    return response.send();
 }
