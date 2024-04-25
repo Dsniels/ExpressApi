94a1f99f9cf259226ecff461319516594e420e70
Daniel Salazar <dsnile.salazr@gmail.com>
2024-04-24 21:03:18 -0600

    Update authentication roles in Orden routes

--- Backend/Routes/Api/Orden/index.js
+++ Backend/Routes/Api/Orden/index.js
@@ -3,10 +3,10 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../Auth/auth.service');
 
-router.get('/', auth.hasRole("manager") ,controller.mostrarOrdenes);
+router.get('/', auth.hasRole("user") ,controller.mostrarOrdenes);
 router.get('/:id', auth.hasRole("user"), controller.mostrarOrdenPorId);
-router.put('/actualizar/:id', auth.hasRole("manager"), controller.updateOrden);
-router.delete('/eliminar/:id', auth.hasRole("manager")  , controller.eliminarOrden);
+router.put('/actualizar/:id', auth.hasRole("user"), controller.updateOrden);
+router.delete('/eliminar/:id', auth.hasRole("user")  , controller.eliminarOrden);
 router.post('/crearorden', auth.hasRole("user") ,controller.crearOrden);
 
 module.exports = router;
