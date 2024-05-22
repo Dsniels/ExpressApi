ab2f35b58c0704886428088480306f37e20fe637
Daniel Salazar <dsnile.salazr@gmail.com>
2024-05-21 23:57:45 -0600

    chore: remove unused passport session middleware

--- Backend/server.js
+++ Backend/server.js
@@ -22,7 +22,7 @@ app.use(cors())
 
 app.use(bodyParser.json())
 app.use(passport.initialize())
-app.use(passport.session());
+
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*')
