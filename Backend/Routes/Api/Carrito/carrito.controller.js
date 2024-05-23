014f5a37cdf4245f2a06de7f9ffc072a9190f43c
Daniel Salazar <dsnile.salazr@gmail.com>
2024-05-23 10:16:35 -0600

    chore: update carrito controller to store items in Redis

--- Backend/Routes/Api/Carrito/carrito.controller.js
+++ Backend/Routes/Api/Carrito/carrito.controller.js
@@ -15,15 +15,22 @@ exports.obtenerCarritoId = async (request, response) => {
 }
 
 exports.updateCarrito = async (request, response) => {
+
   try {
+
     const items = JSON.stringify(request.body.items)
-    const carrito = await redis.set(
-      `carrito:${(request.user._id.toHexString(), items)}`
+    console.timeLog('carrito item', items);
+    //const carritos = redis.SET();
+    //const carritos = redis.set()
+    const carrito = await redis.setEx(
+      `${JSON.stringify(request.user._id.toHexString())}`, 3000 ,items
     )
+    console.log('carritostring');
+    console.log(carrito);
     if (!carrito) throw new Error('Error al actualizar el carrito')
     return response
       .status(200)
-      .send(carrito)
+      
       .JSON({ message: 'carrito actualizado' })
   } catch (error) {
     return response.send(error)
