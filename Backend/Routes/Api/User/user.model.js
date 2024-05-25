0d28ce37e274da21126e8b4d07b8b6a2b6a38048
Daniel Salazar <dsnile.salazr@gmail.com>
2024-05-25 14:08:44 -0600

    chore: Update user model to include googleId field

--- Backend/Routes/Api/User/user.model.js
+++ Backend/Routes/Api/User/user.model.js
@@ -16,7 +16,6 @@ const UserSchema = new Schema({
   },
   password: {
     type: String,
-    required: true
   },
   role: {
     type: String,
@@ -31,6 +30,9 @@ const UserSchema = new Schema({
     type: String,
     default: '',
     required: false
+  },
+  googleId : {
+    type : String
   }
 })
 
