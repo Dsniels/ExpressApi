ad2dd7eb1ee103eb9c9a43307b08f463372b145a
Daniel Salazar <dsnile.salazr@gmail.com>
2024-05-25 14:08:17 -0600

    chore: Update passport.js with Google OAuth2 strategy and user model

--- Backend/Routes/Api/Auth/passport.js
+++ Backend/Routes/Api/Auth/passport.js
@@ -1,6 +1,6 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth2').Strategy;
-
+const User = require('../User/user.model');
 
 passport.use( 
     new GoogleStrategy({
@@ -9,19 +9,30 @@ passport.use(
         callbackURL : '/api/users/google/callback',
         scope : ['profile', 'email']
     },
-    (access_token, refresh_token, profile, callback) => {
-        callback(null, profile);
+    (access_token, refresh_token, profile, done) => {
+        User.findOne({googleId : profile.id})
+            .then(user => {
+              user ? done(null, user) : new User({
+                googleId : profile.id, 
+                name : profile.name, 
+                email : profile.email
+              })
+                .save()
+                .then(user=>done(null, user));
+            })
     })    
   );
 
 
   passport.serializeUser((user, done) => {
-    done(null,user);
+    done(null,user.id);
   });
 
-passport.deserializeUser((user, done) => {
-    done(null,user);
+passport.deserializeUser((id, done) => {
+    User.findById(id).then(user => done(null, user));
 });
+
+
 
 
 
