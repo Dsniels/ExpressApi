fb96223f8bdea85b9b20c63130beb4005e8a025f
Daniel Salazar <dsnile.salazr@gmail.com>
2024-05-28 01:25:22 -0600

    chore: Make lastname field optional in user model

--- Backend/Routes/Api/User/user.model.js
+++ Backend/Routes/Api/User/user.model.js
@@ -4,15 +4,13 @@ const Schema = mongoose.Schema
 const UserSchema = new Schema({
   name: {
     type: String,
-    required: true
+
   },
   lastname: {
     type: String,
-    required: false 
   },
   email: {
     type: String,
-    required: true
   },
   password: {
     type: String,
