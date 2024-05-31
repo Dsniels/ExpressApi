3de2c417b2a214bfac96650028a141e9866f0ae6
Daniel Salazar <dsnile.salazr@gmail.com>
2024-05-30 19:56:44 -0600

    feat: Agregar modelo de dirección en el backend

---
+++ Backend/Routes/Api/Direccion/direccion.model.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+
+const DireccionSchema = new Schema({
+Estado: {
+    type: String
+},
+Ciudad: {
+    type: String
+},
+CodigoPostal: {
+    type: Number
+},
+Colonia: {
+    type: String
+},
+Calle: {
+    type: String
+},
+Pais : {
+    type : String
+},
+User : {
+    type : Schema.ObjectId,
+    ref : 'User'
+}
+
+})
+
+module.exports = Direccion = mongoose.model('Direccion', DireccionSchema);
+
+
