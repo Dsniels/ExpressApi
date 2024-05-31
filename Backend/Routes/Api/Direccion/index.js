08e406d1f16d9c7f70f42dbddd40ee19ae6e8b12
Daniel Salazar <dsnile.salazr@gmail.com>
2024-05-30 19:56:52 -0600

    feat: Agregar ruta de actualización de dirección en el backend

---
+++ Backend/Routes/Api/Direccion/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const controller = require('./direccion.controller');
+const { hasRole } = require('../Auth/auth.service');
+
+
+router.put('/actualizarDireccion', hasRole('user'), controller.ActualizarDireccion);
+
+
+module.exports = router;
