c35425cb6bdcf594d4d97e269c1d6fd7d9b2bed2
Daniel Salazar <dsnile.salazr@gmail.com>
2024-06-02 00:41:11 -0600

    feat: Update Google authentication routes and callbacks

--- Backend/Routes/Api/User/index.js
+++ Backend/Routes/Api/User/index.js
@@ -12,7 +12,7 @@ router.post('/login', controller.loginUser);
 
 router.get('/done', controller.done);
 
-router.get('/google', passport.authenticate('google', ['profile', 'email']));
+router.get('/google', passport.authenticate('google',{session : false,scope : ['profile', 'email']} ));
 
 router.get('/all', auth.hasRole('manager'), controller.showUsers);
 
@@ -20,7 +20,7 @@ router.get('/:id', auth.hasRole('manager'), controller.show);
 
 router.put('/actualizar/:id', auth.hasRole('user'), controller.updateUser);
 
-router.get('/google/callback',passport.authenticate('google',{successRedirect: 'http://localhost:3000/api/users/done',failureRedirect : '/login'}),controller.AuthGoogle);
+router.get('/google/callback',passport.authenticate('google',{ session : false,successRedirect: 'http://localhost:3000/',failureRedirect : '/login'}),controller.AuthGoogle);
 
 
 module.exports = router
